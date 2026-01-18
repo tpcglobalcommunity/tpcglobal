2d1a574ef0f69a4c814e464521bf2482bb22da4e
ekodaeng <72572256+ekodaeng@users.noreply.github.com>
2026-01-19 04:38:16 +0800

    Updated sw.js

--- public/sw.js
+++ public/sw.js
@@ -6,6 +6,7 @@ self.addEventListener('activate', event => {
   event.waitUntil(self.clients.claim());
 });
 
+// Jangan cache request non-http(s)
 self.addEventListener('fetch', event => {
   const url = new URL(event.request.url);
   if (url.protocol !== 'http:' && url.protocol !== 'https:') return;
