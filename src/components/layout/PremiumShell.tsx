3ab35d6d7e08c0c70e34eec58cfc42cc1c406275
ctgold <ctgold@users.noreply.github.com>
2026-01-31 02:16:58 +0800

    fix(auth): remove duplicate header on login - remove PublicHeader from PremiumShell

--- src/components/layout/PremiumShell.tsx
+++ src/components/layout/PremiumShell.tsx
@@ -1,5 +1,4 @@
 import { ReactNode } from "react";
-import { PublicHeader } from "./PublicHeader";
 import { Footer } from "./Footer";
 import { BottomNav } from "./BottomNav";
 
@@ -11,7 +10,6 @@ interface PremiumShellProps {
 export const PremiumShell = ({ children, showBottomNav = true }: PremiumShellProps) => {
   return (
     <div className="min-h-screen flex flex-col">
-      <PublicHeader />
       <main className="flex-1 pb-20 lg:pb-0">
         {children}
       </main>
